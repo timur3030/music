4d285544c630be9d9db182a72ec574b63c9a5c10
Timur <timur87@mail.ru>
2024-03-20 01:40:54 +0300

    feat: add play pause btn work

--- js/script.js
+++ js/script.js
@@ -3,22 +3,26 @@ import { songs } from "./songs.js";
 document.addEventListener("DOMContentLoaded", function () {
   let audio = document.querySelector("audio");
   let h1 = document.querySelector("h1");
+  let start = document.querySelector(".start");
   let play = document.querySelector(".play");
   let pause = document.querySelector(".pause");
   let prev = document.querySelector(".prev");
   let isPlaying = false;
   let isPaused = false;
   let isStopped = false;
-  console.log(songs);
-  play.addEventListener("click", () => {
-    audio.play();
-    console.log("play");
+
+  play.addEventListener("click", (e) => {
+    if (e.target.classList.contains("pause")) {
+      e.target.innerHTML = '<img src="icons/play.svg" alt="" />';
+      audio.pause();
+    } else {
+      e.target.innerHTML = '<img src="icons/pause.svg" alt="" />';
+      audio.play();
+    }
+    play.classList.toggle("pause");
   });
-  pause.addEventListener("click", () => {
-    audio.pause();
-    console.log("pause");
-  });
-  console.log(play);
-  console.log(pause);
+
+  // console.log(play);
+  // console.log(pause);
   // console.log("Audio Player");
 });

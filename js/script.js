3e4c882ee6209344746551c3bcfce9a1e61da396
Timur <timur87@mail.ru>
2024-03-19 04:33:22 +0300

    fix: controls btns

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+import { songs } from "./songs.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   let audio = document.querySelector("audio");
   let h1 = document.querySelector("h1");
+  let play = document.querySelector(".play");
+  let pause = document.querySelector(".pause");
+  let prev = document.querySelector(".prev");
   let isPlaying = false;
   let isPaused = false;
   let isStopped = false;
-  h1.addEventListener("click", () => {
+  console.log(songs);
+  play.addEventListener("click", () => {
     audio.play();
     console.log("play");
-    console.log(audio);
   });
-  // console.log(audio);
+  pause.addEventListener("click", () => {
+    audio.pause();
+    console.log("pause");
+  });
+  console.log(play);
+  console.log(pause);
   // console.log("Audio Player");
 });
---
+++ js/songs.js
@@ -0,0 +1,32 @@
+export const songs = [
+  {
+    img: "img/fire.png",
+    name: "Play with Fire",
+    artist: "Sam Tinnesz",
+    music: "music/play_with_fire.mp3",
+  },
+  {
+    img: "img/fever_ray.png",
+    name: "If I had a Heart",
+    artist: "Fever Ray",
+    music: "music/heart.mp3",
+  },
+  {
+    img: "img/viking.png",
+    name: "My Mother Told Me",
+    artist: "Sea Shanty",
+    music: "music/mother.mp3",
+  },
+  {
+    img: "img/come_with.png",
+    name: "Come with Me Now",
+    artist: "Kongos",
+    music: "music/come_with.mp3",
+  },
+  {
+    img: "img/unstoppable.png",
+    name: " I am Unstoppable",
+    artist: "The Score",
+    music: "music/unstoppable.mp3",
+  },
+];
